731c53faf2315c9bfdbfcc3532c3f5b788f85dca
Robinson Regalado <rregalado17@gmail.com>
2019-07-30 21:20:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,19 @@ class Triangle
   def kind()
     if side_a <= 0 || side_b <= 0 || side_c <= 0 
       raise TriangleError
-    elsif 
+    elsif (@side_a+@side_b <= @side_c) || (@side_a+@side_c <= @side_b) || (@side_b+@side_c <= @side_a)
+      raise TriangleError
+    else
+      if (@side_a == @side_b) && (@side_b == @side_c)
+        :equilateral
+      elsif (@side_a == @side_b) || (@side_b == @side_c) || (@side_a == @side_c)
+        :isosceles
+      elsif (@side_a != @side_b) && (@side_b != @side_c) && (@side_a != @side_c)
+        :scalene
+      end
+    end
+
+  end
       
     
     
