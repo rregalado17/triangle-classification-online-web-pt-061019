aa45cc94de609615b317eec79d7bfce685f1b479
Robinson Regalado <rregalado17@gmail.com>
2019-07-30 21:19:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c
+  
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+  
+  def kind()
+    if side_a <= 0 || side_b <= 0 || side_c <= 0 
+      raise TriangleError
+    elsif 
+      
+    
+    
+    
+  end
+
+
+  class Triangle < StandardError
+    
+  end
+
 end
